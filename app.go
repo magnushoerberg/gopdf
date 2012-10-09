b31592e35e5b125e18f417d9f70c6f2cc7e9df26
Magnus Hörberg <magnus.hoerberg@gmail.com>
2012-10-09 16:23:46 +0200

    remove channels and gorutines

--- app.go
+++ app.go
@@ -3,11 +3,11 @@ import (
   "bytes"
   "os/exec"
 )
-func PDF(input string) bytes.Buffer {
+func PDF(input string) *bytes.Buffer {
     cmd := exec.Command("wkhtmltopdf", "-", "-")
     cmd.Stdin = bytes.NewBufferString(input)
     var out bytes.Buffer
     cmd.Stdout = &out
-    go cmd.Run()
-    return out
+    cmd.Run()
+    return &out
 }
